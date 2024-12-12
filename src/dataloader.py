a322d11ad2951c73b49090dc4db673f198b9ef42
LucianCrainic <crainic.lucian@gmail.com>
2024-12-13 00:57:25 +0100

    [ADD] init python files to fix import problems

--- src/dataloader.py
+++ src/dataloader.py
@@ -10,9 +10,19 @@ class DataLoader:
         self.id_mapping = id_mapping
     
     def generate_data(self):
+        """
+        Generates data by iterating over image paths and processing each image.
+        """
         for image_path in self.image_paths:
-            patient_id = os.path.basename(image_path).split("_")[0]
-            label = self.id_mapping[patient_id]
-            processor = DataProcessor()
-            vein_image = processor.preprocess_image(image_path)
-            yield vein_image, label
+            vein_image, label, _ = self.generate_image(image_path)
+            return vein_image, label
+    
+    def generate_image(self, image_path):
+        """
+        Generates a preprocessed vein image along with its label and patient ID.
+        """
+        processor = DataProcessor()
+        patient_id = os.path.basename(image_path).split("_")[0]
+        label = self.id_mapping[patient_id]
+        vein_image = processor.preprocess_image(image_path)
+        return vein_image, label, patient_id
