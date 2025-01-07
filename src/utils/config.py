e10fd8d916a7a77ada3a3936856d9578b7c25047
LucianCrainic <crainic.lucian@gmail.com>
2025-01-07 21:45:12 +0100

    [REFACTOR] clerical

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,10 +1,5 @@
-# Set the path to the dataset where the images are stored based on your local setup.
 DATASET_PATH = "/home/lucian/University/MSc-Courses/BiometricSystems/data/"
-# Refers to the hand - left or right. [l, r]
 HAND = "l"
-# Refers to the spectrum of the image. [940, 850, 700, 630, 460, WHT]
 SPECTRUM = "850"
-# Seed for reproducibility.
 SEED = 42
-# NOTE: do not change the following values.
 PATIENTS = [f"{i:03}" for i in range(1, 101)]
